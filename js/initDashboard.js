bf21c36ca426ef6d3f10a302dbc86605e2892ccb
stu1130 <gstu1130@gmail.com>
2017-11-23 14:52:41 -0800

    javascript with localStorage

---
+++ js/initDashboard.js
@@ -0,0 +1,54 @@
+$(function() {
+    function init() {
+        let status = localStorage.getItem("status");
+        let events = localStorage.getItem("events");
+        let courses = localStorage.getItem("courses");
+
+        let eventsDom = $(".collapsible li");
+        let coursesDom = $("#courseList");
+        if (status === "tutor" && events === "default" && courses === "default") {
+            eventsDom.append("<div class='collapsible-header'>No upcoming events!</div>");
+            coursesDom.append('<div class="card blue-grey darken-1">\
+                <div class="card-content white-text">\
+                <span class="card-title" >No courses</span>\
+                </div></div>');
+        } else if (status === "tutor" && events === "meeting" && courses === "registed") {
+            eventsDom.append("<div class='collapsible-header'>1:1 meeting with Andrew</div>");
+            coursesDom.append('<div class="card blue-grey darken-1">\
+            <div class="card-content white-text" >\
+                <span class="card-title" ><a class="grey-text text-lighten-3" href="CC_Tutor.html">\
+                <i class="material-icons">cloudy</i>C1. Cloud Computing</a></span>\
+            </div></div>');
+        } else if (status === "student" && events === "default" && courses === "default") {
+            eventsDom.append('<div class="collapsible-header">Cloud Computing Phase 3 Due Nov 10,3 pm</div>');
+            coursesDom.append('<div class="card blue-grey darken-1">\
+                <div class="card-content white-text" >\
+                <span class="card-title" ><a class="grey-text text-lighten-3" href="CC_Tutor.html">\
+                <i class="material-icons"></i>C4. Machine Learning</a></span>\
+                </div></div>');
+        } else if (status === "student" && events === "meeting" && courses === "default") {
+            eventsDom
+                .append('<div class="collapsible-header">Meeting with Professor Emily Nov 10,12 pm</div>')
+                .append('<div class="collapsible-header">Cloud Computing Phase 3 Due Nov 10,3 pm</div>');
+            coursesDom
+                .append('<div class="card blue-grey darken-1">\
+                    <div class="card-content white-text" >\
+                    <span class="card-title" ><a class="grey-text text-lighten-3" href="CC_Tutor.html">\
+                    <i class="material-icons">cloud</i>C1. Cloud Computing</a></span>\
+                    </div></div>')
+                .append('<div class="card blue-grey darken-1">\
+                    <div class="card-content white-text" >\
+                    <span class="card-title" ><a class="grey-text text-lighten-3" href="CC_Tutor.html">\
+                    <i class="material-icons">insert_chart</i>C4. Machine Learning</a></span>\
+                    </div></div>')
+                .append('<div class="card blue-grey darken-1">\
+                    <div class="card-content white-text" >\
+                    <span class="card-title" ><a class="grey-text text-lighten-3" href="CC_Tutor.html">\
+                    <i class="material-icons">trending_up</i>C5. An Intuitive Introduction to Probability</a></span>\
+                    </div></div>');
+                    
+        }
+    
+    }
+    init();
+});
