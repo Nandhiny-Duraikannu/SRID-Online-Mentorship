b3e59c6e0576962c06628044eb727f43713c4321
Nandhiny-Duraikannu <nduraika@andrew.cmu.edu>
2017-11-26 09:04:56 -0800

    Bug Fix - card width not aligned with button width - revert

--- js/initDashboard.js
+++ js/initDashboard.js
@@ -8,13 +8,13 @@ $(function() {
         let coursesDom = $("#courseList");
         if (status === "tutor" && events === "default" && courses === "default") {
             eventsDom.append("<div class='collapsible-header'>No upcoming events!</div>");
-            coursesDom.append('<div class="card blue-grey darken-1 row">\
+            coursesDom.append('<div class="card blue-grey darken-1">\
                 <div class="card-content white-text">\
                 <span class="card-title" >No courses</span>\
                 </div></div>');
         } else if (status === "tutor" && events === "meeting" && courses === "default") {
             eventsDom.append("<div class='collapsible-header'><a href='meeting-detail.html'>1:1 meeting with Andrew</a></div>");
-            coursesDom.append('<div class="card blue-grey darken-1 row">\
+            coursesDom.append('<div class="card blue-grey darken-1">\
             <div class="card-content white-text" >\
                 <span class="card-title" ><a class="grey-text text-lighten-3" href="CC_Tutor.html">\
                 <i class="material-icons">cloud</i> sC1. Cloud Computing</a></span>\
@@ -22,14 +22,14 @@ $(function() {
         } else if (status === "tutor" && events === "default" && courses === "addedCourse") {
             Materialize.toast('Successfully register the course!', 5000);
             eventsDom.append("<div class='collapsible-header'>No upcoming events!</div>");
-            coursesDom.append('<div class="card blue-grey darken-1 row">\
+            coursesDom.append('<div class="card blue-grey darken-1">\
             <div class="card-content white-text" >\
                 <span class="card-title" ><a href="course-dashboard.html" class="grey-text text-lighten-3" href="CC_Tutor.html">\
                 <i class="material-icons">cloud</i> C1. Cloud Computing</a></span>\
             </div></div>');
         } else if (status === "tutor" && events === "meeting" && courses === "addedCourse") {
             eventsDom.append("<div class='collapsible-header'><a href='meeting-detail.html'>1:1 meeting with Andrew</a></div>");
-            coursesDom.append('<div class="card blue-grey darken-1 row">\
+            coursesDom.append('<div class="card blue-grey darken-1">\
             <div class="card-content white-text" >\
                 <span class="card-title" ><a href="course-dashboard.html" class="grey-text text-lighten-3" href="CC_Tutor.html">\
                 <i class="material-icons">cloud</i> C1. Cloud Computing</a></span>\
@@ -38,17 +38,17 @@ $(function() {
             eventsDom
                 .append('<div class="collapsible-header">Cloud Computing Phase 3 Due Nov 10,3 pm</div>');
             coursesDom
-                .append('<div class="card blue-grey darken-1 row">\
+                .append('<div class="card blue-grey darken-1">\
                     <div class="card-content white-text" >\
                     <span class="card-title" ><a class="grey-text text-lighten-3" href="course-dashboard.html">\
                     <i class="material-icons">cloud</i> C1. Cloud Computing</a></span>\
                     </div></div>')
-                .append('<div class="card blue-grey darken-1 row">\
+                .append('<div class="card blue-grey darken-1">\
                     <div class="card-content white-text" >\
                     <span class="card-title" ><a class="grey-text text-lighten-3">\
                     <i class="material-icons">insert_chart</i> C4. Machine Learning</a></span>\
                     </div></div>')
-                .append('<div class="card blue-grey darken-1 row">\
+                .append('<div class="card blue-grey darken-1">\
                     <div class="card-content white-text" >\
                     <span class="card-title" ><a class="grey-text text-lighten-3">\
                     <i class="material-icons">trending_up</i> C5. An Intuitive Introduction to Probability</a></span>\
@@ -59,17 +59,17 @@ $(function() {
                 .append('<div class="collapsible-header"><a href="meeting-detail.html">1:1 meeting with Emily</a></div>')
                 .append('<div class="collapsible-header">Cloud Computing Phase 3 Due Nov 10,3 pm</div>');
             coursesDom
-                .append('<div class="card blue-grey darken-1 row">\
+                .append('<div class="card blue-grey darken-1">\
                     <div class="card-content white-text" >\
                     <span class="card-title" ><a class="grey-text text-lighten-3" href="course-dashboard.html">\
                     <i class="material-icons"> cloud</i>C1. Cloud Computing</a></span>\
                     </div></div>')
-                .append('<div class="card blue-grey darken-1 row">\
+                .append('<div class="card blue-grey darken-1">\
                     <div class="card-content white-text" >\
                     <span class="card-title" ><a class="grey-text text-lighten-3"">\
                     <i class="material-icons"> insert_chart</i>C4. Machine Learning</a></span>\
                     </div></div>')
-                .append('<div class="card blue-grey darken-1 row">\
+                .append('<div class="card blue-grey darken-1">\
                     <div class="card-content white-text" >\
                     <span class="card-title" ><a class="grey-text text-lighten-3"">\
                     <i class="material-icons"> trending_up</i>C5. An Intuitive Introduction to Probability</a></span>\
