5245b2e76ca2652f290247b717f7fc886d3f1fdb
stu1130 <gstu1130@gmail.com>
2017-11-26 23:38:48 -0800

    add profile

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,6 +1,19 @@
 (function ($) {
   $(function () {
     $('.button-collapse').sideNav();
+    const nameDom = $("#userName");
+    const emailDom = $("#userEmail");
+    const status = localStorage.getItem("status");
+
+    if (status === "tutor") {
+      nameDom.text("Emily");
+      emailDom.text("emily0604@cmu.edu");
+    } else {
+      nameDom.text("Andrew");
+      emailDom.text("andrew1212@cmu.edu");
+    }
+    
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
