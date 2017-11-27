4ef3fd4e32a7d9ac2442f2724a7d3f98dc690779
stu1130 <gstu1130@gmail.com>
2017-11-26 16:01:32 -0800

    complete the button width

--- js/initCourseDashboard.js
+++ js/initCourseDashboard.js
@@ -4,18 +4,18 @@ $(function() {
         let buttonDom = $('#courseDashboardButton');
         if (status === "tutor") {
             buttonDom
-                .append('<a class="waves-effect waves-light btn col s12">lecture videos</a>')
-                .append('<a class="waves-effect waves-light btn col s12">create quiz</a>')
-                .append('<a href="student-list.html" class="waves-effect waves-light btn col s12">student list</a>')
-                .append('<a href="view-transcript.html" class="waves-effect waves-light btn col s12">view transcript</a>')
+                .append('<a class="row waves-effect waves-light btn col s12">lecture videos</a>')
+                .append('<a class="row waves-effect waves-light btn col s12">create quiz</a>')
+                .append('<a href="student-list.html" class="row waves-effect waves-light btn col s12">student list</a>')
+                .append('<a href="view-transcript.html" class="row waves-effect waves-light btn col s12">view transcript</a>')
         } else {
             buttonDom
-                .append('<a class="waves-effect waves-light btn col s12" onclick="viewVideo()">lecture videos</a>')
-                .append('<a href="view-grades.html" class="waves-effect waves-light btn col s12">view grades</a>')
-                .append('<a class="waves-effect waves-light btn col s12">take quiz</a>')
-                .append('<a class="waves-effect waves-light btn col s12">forum</a>')
-                .append('<a href="schedule-meeting.html" class="waves-effect waves-light btn col s12">scedule 1:1 meeting</a>')
-                .append('<a href="view-transcript.html" class="waves-effect waves-light btn col s12">view meeting transcripts</a>');
+                .append('<a class="row waves-effect waves-light btn col s12" onclick="viewVideo()">lecture videos</a>')
+                .append('<a href="view-grades.html" class="row waves-effect waves-light btn col s12">view grades</a>')
+                .append('<a class="row waves-effect waves-light btn col s12">take quiz</a>')
+                .append('<a class="row waves-effect waves-light btn col s12">forum</a>')
+                .append('<a href="schedule-meeting.html" class="row swaves-effect waves-light btn col s12">scedule 1:1 meeting</a>')
+                .append('<a href="view-transcript.html" class="row waves-effect waves-light btn col s12">view meeting transcripts</a>');
         }
     }
     init();
