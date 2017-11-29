29166f88214aaf6a23666eb28f4553004a677088
stu1130 <gstu1130@gmail.com>
2017-11-28 18:59:14 -0800

    change the profile picture

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,14 +1,17 @@
 (function ($) {
   $(function () {
     $('.button-collapse').sideNav();
+    const photoDom = $("#userImage");
     const nameDom = $("#userName");
     const emailDom = $("#userEmail");
     const status = localStorage.getItem("status");
 
     if (status === "tutor") {
+      photoDom.attr("src", "http://materializecss.com/images/yuna.jpg");
       nameDom.text("Emily (Tutor)");
       emailDom.text("emily0604@cmu.edu");
     } else {
+      photoDom.attr("src", "../images/03.png");
       nameDom.text("Andrew (Student)");
       emailDom.text("andrew1212@cmu.edu");
     }
